f967706d6ff6d3fb239c883bcca91d11a8b9c43e
Hari Earlapati <harihara.earlapati@gmail.com>
2018-08-02 07:48:56 -0500

    Added test classes

---
+++ src/test/java/com/projects/ricefactory/api/OrderControllerTest.java
@@ -0,0 +1,34 @@
+package com.projects.ricefactory.api;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class OrderControllerTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void createOrder() throws Exception {
+    }
+
+    @Test
+    public void updateOrder() throws Exception {
+    }
+
+    @Test
+    public void getOrder() throws Exception {
+    }
+
+    @Test
+    public void getOrders() throws Exception {
+    }
+
+}
---
+++ src/test/java/com/projects/ricefactory/api/RiceTypeControllerTest.java
@@ -0,0 +1,34 @@
+package com.projects.ricefactory.api;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RiceTypeControllerTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void getRiceType() throws Exception {
+    }
+
+    @Test
+    public void getAllRiceTypes() throws Exception {
+    }
+
+    @Test
+    public void createRiceType() throws Exception {
+    }
+
+    @Test
+    public void updateRiceType() throws Exception {
+    }
+
+}
