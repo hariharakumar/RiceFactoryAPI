bad1a29fb22eac0a3ffb1f17d5506c224fe9027c
Hari Earlapati <harihara.earlapati@gmail.com>
2018-08-02 07:50:10 -0500

    Added gradle wrapper to source control

---
+++ src/test/java/com/projects/ricefactory/api/UserControllerTest.java
@@ -0,0 +1,43 @@
+package com.projects.ricefactory.api;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UserControllerTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void getUser() throws Exception {
+    }
+
+    @Test
+    public void searchUsers() throws Exception {
+    }
+
+    @Test
+    public void createUser() throws Exception {
+    }
+
+    @Test
+    public void updateUser() throws Exception {
+    }
+
+    @Test
+    public void deleteUser() throws Exception {
+    }
+
+    @Test
+    public void getAllUsers() throws Exception {
+    }
+
+}
